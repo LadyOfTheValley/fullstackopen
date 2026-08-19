8f5c5a060e10c236c6f12aa9ff297ef9d93e7dd9
LadyOfTheValley <asmaa.rebiha@gmail.com>
2026-08-19 19:36:46 +0100

    1.10+1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,24 +10,34 @@ const Statistics = (props) => {
   } else {
   return(
     <div>
-        <p>good {props.good}</p>
-        <p>neutral {props.neutral}</p>
-        <p>bad {props.bad}</p>
-        <p>All {props.total} </p>
-        <p>average {(props.good+props.neutral*0+props.bad*-1)/props.total}</p>
-        <p>postive {props.good*100/props.total}%</p>
+        <StatisticLine text="good " value={props.good} />
+        <StatisticLine text="neutral " value={props.neutral} />
+        <StatisticLine text="bad " value={props.bad} />
+        <StatisticLine text="total " value={props.total} />
+        <StatisticLine text="average " value={(props.good+props.neutral*0+props.bad*-1)/props.total} />
+        <StatisticLine text="positive " value={`${props.good*100/props.total}%`}/>
     </div>    
   )    
   }
 }
 
-const Header = (props) => {
+const StatisticLine = (props) => {
   return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Button = (props) => {
+  return(
     <div>
-      <h1>{props.course.name}</h1>
+      <button onClick={props.action}>{props.text}</button>
     </div>
   )
 }
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -55,12 +65,12 @@ const App = () => {
   }
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
-      <h1>Statistics</h1>
-      <Statistics good={good} bad={bad} neutral={neutral} total={total} allClicks={allClicks} />
+        <h1>Give Feedback</h1>
+        <Button text="good" action={handleGood} />
+        <Button text="neutral" action={handleNeutral} />
+        <Button text="bad" action={handleBad} />
+        <h1>Statistics</h1>
+        <Statistics good={good} bad={bad} neutral={neutral} total={total} allClicks={allClicks} />
     </div>
   )
 }
