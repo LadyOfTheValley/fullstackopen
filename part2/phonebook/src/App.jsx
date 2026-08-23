a9162b5bc124781ea376431b06e17da7a4836027
LadyOfTheValley <asmaa.rebiha@gmail.com>
2026-08-23 23:57:52 +0100

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 import Filter from './Filter'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   
   const [searchQuery, setSearchQuery] = useState('')
   const handleSearch = (event) => {
@@ -23,6 +19,16 @@ const App = () => {
   const addPerson = (newPerson) => {
     setPersons(persons.concat(newPerson))
   }
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
