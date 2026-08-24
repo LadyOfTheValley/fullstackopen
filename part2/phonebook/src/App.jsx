0898bba1dc31fc29232d2d501c9af321417b006d
LadyOfTheValley <asmaa.rebiha@gmail.com>
2026-08-24 21:02:35 +0100

    2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,10 +3,13 @@ import PersonForm from './PersonForm'
 import Persons from './Persons'
 import Filter from './Filter'
 import personService from './services/personService'
+import Notification from './Notification'
+import './index.css'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [searchQuery, setSearchQuery] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
   useEffect(() => {
     personService
@@ -48,6 +51,10 @@ const App = () => {
           .update(existingPerson.id, updatedPersonObject)
           .then(returnedPerson => {
             setPersons(persons.map(p => (p.id !== existingPerson.id ? p : returnedPerson)))
+            setErrorMessage(`Updated ${existingPerson.name}'s Number`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
           })
       }
       return
@@ -57,12 +64,17 @@ const App = () => {
       .create(personObject)
       .then(returnedPerson => {
         setPersons(persons.concat(returnedPerson))
+        setErrorMessage(`Added ${returnedPerson.name}`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
       })
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter searchQuery={searchQuery} handleSearch={handleSearch}/>
       <h3>add a new</h3>
       <PersonForm persons={persons} onAddPerson={addPerson}/>
