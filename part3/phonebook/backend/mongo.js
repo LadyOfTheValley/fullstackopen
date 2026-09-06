87ccf59c0f5661d06aa089b5eb26b52c23bd04dc
LadyOfTheValley <asmaa.rebiha@gmail.com>
2026-09-06 22:37:29 +0100

    3.12

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://asmaarebiha_db_user:${password}@cluster0.0ge7wn5.mongodb.net/?appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: name,
+    number: number,
+})
+
+if (process.argv.length === 3) {
+Person.find({}).then(result => {
+  console.log('phonebook:')
+  result.forEach(person => {
+    console.log(`${person.name} ${person.number}`)
+  })
+  mongoose.connection.close()
+})    
+}
+
+else if (process.argv.length >= 5) {
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(() => {
+    console.log('Person saved!')
+    mongoose.connection.close()
+  })
+} 
+else {
+  console.log('Please provide both name and number to add an entry.')
+  mongoose.connection.close()
+}
